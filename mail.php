7d965d56c2208b55b9a38244f0c3819447b351e7
Anrew Crandall <andrewcrandall4040@gmail.com>
2022-08-18 11:45:58 -0600

    mail #2

---
+++ mail.php
@@ -0,0 +1,20 @@
+
+<?php
+//Code Recieved from Ajith Jojo Joseph
+//https://codeconia.com/contact-form-with-phpmail-for-your-website/
+
+//get data from form  
+$name = $_POST['name'];
+$email= $_POST['email'];
+$message= $_POST['message'];
+$to = "andrewcrandall4040@gmail.com";
+$subject = "Mail From Portfolio";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+$headers = "From: noreply@yoursite.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+} 
+//redirect
+header("Location:index.html");
+?>
